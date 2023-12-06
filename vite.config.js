e9debc0f7fb7fb1a6406f7304f0eccd05d09e0ea
keerthikam_pfw <mulak01@pfw.edu>
2023-12-06 15:49:49 -0500

    added deployment code fixes

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+const config = {
+  build: {
+    rollupOptions: {
+      input: 'components/Hero/index.tsx', // Adjust this to your actual entry point
+    },
+  },
+};
+
+export default config;

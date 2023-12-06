7b88e80e49695fee779b0bab883515f0a29cd6f6
keerthikam_pfw <mulak01@pfw.edu>
2023-12-06 17:25:11 -0500

    added code fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withImages = require('next-images');
+
+module.exports = withImages({
   images: {
     domains: ["localhost"],
     remotePatterns: [
@@ -10,6 +11,35 @@ const nextConfig = {
       },
     ],
   },
-};
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  // Tailwind CSS configuration
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer'),
+            ],
+          },
+        },
+      ],
+    });
 
-module.exports = nextConfig;
+    return config;
+  },
+});
+
+// If you have other plugins or configurations, you can extend them here.
